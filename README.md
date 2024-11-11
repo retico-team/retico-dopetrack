ebd2a5154fa69f9c0f5973a1e6f789e6c6a64ccd
porterrigby <porterrigby@u.boisestate.edu>
2024-11-11 10:13:42 -0700

    Adds retico-clip style README

---
+++ README.md
@@ -0,0 +1,55 @@
+# retico-clip
+A ReTiCo module for CLIP. See below for more information.
+
+### Installation and requirements
+
+See https://github.com/naver/dope 
+
+Required Packages:
+- pytorch
+- torchvision
+- opencv
+- numpy
+
+
+### Example
+```python
+import sys, os
+
+os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
+
+prefix = '/path/to/prefix/'
+sys.path.append(prefix+'retico-core')
+sys.path.append(prefix+'retico-vision')
+
+from retico_vision.vision import WebcamModule 
+from retico_core.debug import DebugModule
+from dopetrack import DopeTrackingModule
+
+cam = WebcamModule()
+dope = DopeTrackingModule(visualizer=True)
+debug = DebugModule(print_payload_only=True)
+
+cam.subscribe(dope)
+dope.subscribe(debug)
+
+cam.run()
+dope.run()
+debug.run()
+
+input()
+
+debug.stop()
+dope.stop()
+cam.stop()
+```
+
+Citation
+```
+@inproceedings{dope,
+  title={{DOPE: Distillation Of Part Experts for whole-body 3D pose estimation in the wild}},
+  author={{Weinzaepfel, Philippe and Br\'egier, Romain and Combaluzier, Hadrien and Leroy, Vincent and Rogez, Gr\'egory},
+  booktitle={{ECCV}},
+  year={2020}
+}
+```
--- dopetrack.py
+++ dopetrack.py
@@ -9,7 +9,7 @@ import threading
 from collections import deque
 
 from src import postprocess, visu3d
-from src.dope import visu
+from src import visu
 from src.model import dope_resnet50, num_joints
 
 import retico_core
@@ -37,7 +37,7 @@ class DopeTrackingModule(retico_core.AbstractModule):
     def output_iu():
         return PosePositionsIU
 
-    def __init__(self, visualizer=False, visualizer3D=False, **kwargs):
+    def __init__(self, visualizer=False, **kwargs):
         """
 
         :param visualizer: boolean to determine if webcam output with annotations is shown
@@ -45,15 +45,12 @@ class DopeTrackingModule(retico_core.AbstractModule):
         """
 
         super().__init__(**kwargs)
+        self._run_tracker_active = False
         self.visualizer = visualizer
-        self.visualizer3D = visualizer3D
-
         self.queue = deque(maxlen=1)
-
         self._thisdir = path.realpath(path.dirname(__file__))
 
-
-    def detect_pose(self, image, modelname='DOPErealtime_v1_0_0', postprocessing='ppi'):
+    def detect_pose(self, image, modelname='DOPErealtime_v1_0_0', postprocessing='ppi', half=True):
         """
         Perform pose detection on an image.
         """
@@ -63,12 +60,9 @@ class DopeTrackingModule(retico_core.AbstractModule):
             try:
                 from src.lcr_net_ppi_improved import LCRNet_PPI_improved
             except ModuleNotFoundError:
-                raise Exception(
-                    'To use the pose proposals integration (ppi) as postprocessing, please follow the '
-                    'readme instruction by cloning our modified version of LCRNet_v2.0 here. Alternatively, '
-                    'you can use --postprocess nms without any installation, with a slight decrease of performance.'
-                )
+                print("Failed to import PPI module. Using built-in NMS instead.")
 
+        # use GPU if available, else use CPU
         device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
         # load model
@@ -77,10 +71,16 @@ class DopeTrackingModule(retico_core.AbstractModule):
             raise Exception(
                 f'{ckpt_fname} does not exist, please download the model first and place it in the models/ folder'
             )
+
         print('Loading model', modelname)
         ckpt = torch.load(ckpt_fname, map_location=device)
-        # ckpt['half'] = False # uncomment this line in case your device cannot handle half computation
+
+
+        # Half computation is true by default. Change to false in case your device cannot handle half computation
+        # Sets half computation usage
+        ckpt['half'] = half
         ckpt['dope_kwargs']['rpn_post_nms_top_n_test'] = 1000
+
         model = dope_resnet50(**ckpt['dope_kwargs'])
         if ckpt['half']: model = model.half()
         model = model.eval()
@@ -88,17 +88,17 @@ class DopeTrackingModule(retico_core.AbstractModule):
         model = model.to(device)
 
         # load image
-        print('Loading image', image)
+        print('Loading image')
         imlist = [ToTensor()(image).to(device)]
         if ckpt['half']: imlist = [im.half() for im in imlist]
         resolution = imlist[0].size()[-2:]
 
-        #forward pass
+        # forward pass
         print('Running DOPE')
         with torch.no_grad():
             results = model(imlist, None)[0]
 
-        # postporcess results (pose proposals integration, wrists/head assignment
+        # postprocess results (pose proposals integration, wrists/head assignment)
         print('Postprocessing')
         assert postprocessing in ['nms', 'ppi']
         parts = ['body', 'hand', 'face']
@@ -135,32 +135,7 @@ class DopeTrackingModule(retico_core.AbstractModule):
                     for i in range(len(detections[part])):
                         detections[part][i]['hand_isright'] = bestcls < ckpt['hand_ppi_kwargs']['K']
 
-        # Assignment of hands and head to body
-        detections, body_with_wrists, body_with_head = postprocess.assign_hands_and_head_to_body(detections)
-
-        if self.visualizer:
-            print('Displaying results')
-            det_poses2d = {part: np.stack([d['pose2d'] for d in part_detections], axis=0) if len(part_detections) > 0 else np.empty((0, num_joints[part], 2), dtype=np.float32) for part, part_detections in detections.items()}
-            scores = {part: [d['score'] for d in part_detections] for part, part_detections in detections.items()}
-            output_image = visu.visualize_bodyhandface2d(np.asarray(image)[:, :, ::-1], det_poses2d, dict_scores=scores)
-            return output_image, detections
-        elif self.visualizer3D:
-            print('Displaying results in 3D')
-            viewer3d = visu3d.Viewer3d()
-            img3d, img2d = viewer3d.plot3d(
-                image,
-                bodies={'pose3d': np.stack([d['pose3d'] for d in detections['body']]),
-                        'pose2d': np.stack([d['pose2d'] for d in detections['body']])},
-                hands={'pose3d': np.stack([d['pose3d'] for d in detections['hand']]),
-                       'pose2d': np.stack([d['pose2d'] for d in detections['hand']])},
-                faces={'pose3d': np.stack([d['pose3d'] for d in detections['face']]),
-                       'pose2d': np.stack([d['pose2d'] for d in detections['face']])},
-                body_with_wrists=body_with_wrists,
-                body_with_head=body_with_head,
-                interactive=False
-            )
-            return img2d, detections
-        return detections
+        return postprocess.assign_hands_and_head_to_body(detections)
 
 
     def process_update(self, update_message):
@@ -169,9 +144,7 @@ class DopeTrackingModule(retico_core.AbstractModule):
                 self.queue.append(iu)
 
     def run_tracker(self):
-        # video = cv2.VideoCapture(0)
-
-        while True:
+        while self._run_tracker_active:
             if len(self.queue) == 0:
                 time.sleep(0.05)
                 continue
@@ -190,25 +163,44 @@ class DopeTrackingModule(retico_core.AbstractModule):
             # Resize frame while maintaining aspect ratio
             frame = cv2.resize(frame, (int(frame_width * (640 / frame_height)), 640))
 
-            # Perform pose landmark detection
-            frame, detections = self.detect_pose(frame)
-            print("Detections", detections)
+            # Perform pose detection and assignment of hands and head to body
+            detections, body_with_wrists, body_with_head = self.detect_pose(frame)
 
             # Display frame
             if self.visualizer:
-                cv2.imshow("Pose Detection", frame)
+                print('Displaying results')
+                det_poses2d = {
+                    part: np.stack([d['pose2d'] for d in part_detections], axis=0) if
+                          len(part_detections) > 0 else
+                          np.empty((0, num_joints[part], 2), dtype=np.float32) for
+                          part, part_detections in detections.items()
+                }
+                scores = {
+                    part: [d['score'] for d in part_detections] for
+                          part, part_detections in detections.items()
+                }
+                output_image = visu.visualize_bodyhandface2d(
+                    np.asarray(frame)[:, :, ::-1],
+                    det_poses2d,
+                    dict_scores=scores
+                )
+                cv2.imshow("Pose Detection", output_image)
 
-            # Wait for key press and retrieve ASCII code
-            k = cv2.waitKey(1) & 0xFF
+                # Wait for key press and retrieve ASCII code
+                k = cv2.waitKey(1) & 0xFF
 
-            # Check if key was 'ESC'
-            if k == 27:
-                break
+                # Check if key was 'ESC'
+                if k == 27:
+                    break
 
             output_iu = self.create_iu(input_iu)
-            output_iu.set_landmarks(image, detections, None)
+            output_iu.set_landmarks(frame, detections, None)
             self.append(retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD))
 
     def setup(self):
+        self._run_tracker_active = True
         t = threading.Thread(target=self.run_tracker)
         t.start()
+
+    def shutdown(self):
+        self._run_tracker_active = False
